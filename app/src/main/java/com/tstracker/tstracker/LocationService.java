a897c784e3de967f0bf5e5233256a0300298f769
ali <source.compiler@yahoo.com>
2015-10-20 23:37:15 +0330

    first commit

---
+++ app/src/main/java/com/tstracker/tstracker/LocationService.java
@@ -0,0 +1,129 @@
+package com.tstracker.tstracker;
+
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderApi;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
+import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
+import com.google.android.gms.location.LocationServices;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+/**
+ * Created by ali on 10/17/15.
+ */
+public class LocationService extends Service implements
+        ConnectionCallbacks, OnConnectionFailedListener , com.google.android.gms.location.LocationListener{
+
+   GoogleApiClient mGoogleApiClient;
+    FusedLocationProviderApi fusedLocationProviderApi ;
+   int  LOCATION_INTERVAL=3000;
+    static LocationRequest locationRequest;
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+CreateLocationRequest();
+        fusedLocationProviderApi = LocationServices.FusedLocationApi;
+    }
+    public void CreateLocationRequest(){
+            locationRequest = LocationRequest.create();
+
+            locationRequest.setInterval(LOCATION_INTERVAL);
+            locationRequest.setFastestInterval(LOCATION_INTERVAL);
+            if(Tools.curAccurate==null) {
+                DatabaseHelper dh = new DatabaseHelper(getApplicationContext());
+                SQLiteDatabase db;
+                db = dh.getReadableDatabase();
+                String[] columns = {DatabaseContracts.Settings.COLUMN_NAME_Accurate};
+                Cursor c = db.query(DatabaseContracts.Settings.TABLE_NAME, columns, "", null, "", "", "");
+                c.moveToFirst();
+                Tools.curAccurate=c.getString(c.getColumnIndexOrThrow(DatabaseContracts.Settings.COLUMN_NAME_Accurate));
+            }
+            if ( Tools.curAccurate.contains("l")) {
+                locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+            } else {
+                locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            }
+            Tools.lastAccurate = Tools.curAccurate;
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int s){
+
+    }
+    Location lastLocation;
+    @Override
+    public void onLocationChanged(Location location) {
+        double coarse = 0;
+        if (lastLocation != null)
+            coarse = java.lang.Math.asin((location.getLongitude() - lastLocation.getLongitude()) / (location.getLatitude() - lastLocation.getLatitude()));
+
+        if(location.hasSpeed() || location.distanceTo(lastLocation)>10 || coarse>15) {
+          Tools.SaveLocation(location, coarse);
+          lastLocation = location;
+        }
+        //    Toast.makeText(getApplicationContext(),Tools.curAccurate , Toast.LENGTH_LONG).show();
+
+        Tools.NotificationClass.Notificationm(getApplicationContext(), "رهگیری","در حال ذخیره سازی اطلاعات مکانی شما برای ارسال به سرور.","");
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult cr){
+
+    }
+    @Override
+    public void onConnected(Bundle connectionHint) {
+       // mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        fusedLocationProviderApi.requestLocationUpdates(mGoogleApiClient,  locationRequest,this);
+
+
+    }
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        return null;
+    }
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId){
+
+        if(Tools.curAccurate!= Tools.lastAccurate) {
+            fusedLocationProviderApi.removeLocationUpdates(mGoogleApiClient, this);
+            CreateLocationRequest();
+            fusedLocationProviderApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
+        }
+
+
+        super.onStartCommand(intent, flags, startId);
+
+        return START_STICKY;
+    }
+    @Override
+    public void onCreate() {
+buildGoogleApiClient();
+        super.onCreate();
+        mGoogleApiClient.connect();
+    }
+
+}
--- app/src/main/java/com/tstracker/tstracker/Tools.java
+++ app/src/main/java/com/tstracker/tstracker/Tools.java
@@ -1,6 +1,9 @@
 package com.tstracker.tstracker;
 
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
 import android.location.LocationManager;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
@@ -11,6 +14,12 @@ import android.support.v4.app.TaskStackBuilder;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+
 /**
  * Created by ali on 9/27/15.
  */
@@ -18,7 +27,9 @@ public class Tools {
     public Tools(){
 
     }
-
+    public static Context context;
+    public static  String curAccurate,lastAccurate;
+    public  static  Location currentLocation;
     public  static  String days;
     public  static  String startTime;
     public  static  String EndTime;
@@ -34,21 +45,22 @@ public class Tools {
     }
 public  static  boolean GpsState(Context contxt){
     final LocationManager manager = (LocationManager) contxt.getSystemService(contxt.LOCATION_SERVICE);
-    if (!manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+    if (manager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
         return true;
     }
     else{
         return false;
     }
 
-}public  static  boolean IsNetworProviderEnabled(Context contxt){
+}
+    public  static  boolean IsNetworProviderEnabled(Context contxt){
         final LocationManager manager = (LocationManager) contxt.getSystemService(contxt.LOCATION_SERVICE);
        return manager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
     public  static void CheckGps(Context contxt) {
         boolean buildAlertMessageNoGps=GpsState(contxt);
         final Context con = contxt;
-        if (buildAlertMessageNoGps) {
+        if (!buildAlertMessageNoGps) {
             final AlertDialog.Builder builder = new AlertDialog.Builder(con);
             builder.setMessage("برنامه برای ارسال اطلاعات نیاز دارد تا موقعیت مکانی شما روشن باشد.")
                     .setCancelable(false)
@@ -71,6 +83,69 @@ public  static  boolean GpsState(Context contxt){
         }
     }
 
+
+    private static String IsTimeToSend() {
+        DatabaseHelper dh = new DatabaseHelper(Tools.context);
+        SQLiteDatabase db;
+        try {
+            db = dh.getReadableDatabase();
+            String[] columns = {DatabaseContracts.Settings.COLUMN_NAME_endTime
+                    , DatabaseContracts.Settings.COLUMN_NAME_fromTime
+                    , DatabaseContracts.Settings.COLUMN_NAME_days};
+            Cursor c = db.query(DatabaseContracts.Settings.TABLE_NAME, columns, "", null, "", "", "");
+            c.moveToFirst();
+            try {
+                String days = c.getString(c.getColumnIndexOrThrow(DatabaseContracts.Settings.COLUMN_NAME_days));
+                String EndTime = c.getString(c.getColumnIndexOrThrow(DatabaseContracts.Settings.COLUMN_NAME_endTime));
+                String startTime = c.getString(c.getColumnIndexOrThrow(DatabaseContracts.Settings.COLUMN_NAME_fromTime));
+
+                Calendar cal = Calendar.getInstance();
+                Date date = cal.getTime();
+                String datetime = new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.US).format(date);
+                //(String) DateFormat.format("yyyy-MM-dd HH:mm:ss",c );
+                Integer dayWeek = cal.get(Calendar.DAY_OF_WEEK);
+                int hourOfDay = Integer.valueOf(new SimpleDateFormat("k", Locale.US).format(date));
+
+                if (dayWeek == 7)//shanbe
+                    dayWeek = 0;
+                if (days.contains(String.valueOf(dayWeek)))//check day
+                    if (hourOfDay < Integer.valueOf(EndTime) && hourOfDay >= Integer.valueOf(startTime))//check hour
+                    {
+                        return datetime;
+                    }
+            } catch (Exception er) {
+                // txtResult.setText(er.getMessage());
+            }
+        } catch (Exception ex) {
+
+        }
+        return null;
+    }
+
+    public  static   void SaveLocation(Location location,double _corase){
+
+        currentLocation=location;
+        String Lat, Lon, alti, speed, coarse, datetime;
+        try {
+            Lat = String.valueOf(location.getLatitude());
+            Lon = String.valueOf(location.getLongitude());
+            alti = String.valueOf(location.getAltitude());
+            speed = String.valueOf(location.getSpeed());
+            coarse = String.valueOf(_corase);
+            if(coarse== "NaN")
+                coarse="0";
+            datetime = IsTimeToSend();
+
+            if ( datetime != null  ) {
+
+                SaveGps s = new SaveGps(Tools.context, Lat, Lon, alti, speed, coarse, datetime);
+//              Toast.makeText(getApplicationContext(),datetime, Toast.LENGTH_LONG).show();
+            }
+        } catch (Exception er) {
+String g="";
+        }
+        Lat = Lon = alti = speed = coarse = datetime = null;
+    }
     public static class NotificationClass {
         public static void Notificationm(Context context,String Title,String Details,String packge){
             NotificationCompat.Builder mBuilder =
